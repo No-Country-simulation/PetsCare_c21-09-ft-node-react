9e7d0122b9e8163f53f3d8391deac030307f9742
WalterM <98977639+Mosow357@users.noreply.github.com>
2024-10-16 00:01:39 -0300

    adding colors extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,19 @@ export default {
       colors:{
         'main-blue':' #60A5FA',
         'light-blue': '#60A5FA99',
-        'light-violet': '#AEB0B3'
+        'light-violet': '#AEB0B3',
+        primary: "#2e4053",
+        secondary: "#3498db",
+      }      
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        }
       }
     },
   },

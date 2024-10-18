0c2b0a40a67d155e07a46d75f2a1979f512293ba
WalterM <98977639+Mosow357@users.noreply.github.com>
2024-10-17 21:02:25 -0300

    log profile cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './css/app.css';
-import Home from "./pages/Home";
+import React from "react";
+
+import { div } from "framer-motion/client";
+import logo from "./assets/logoPets.png";
+import Banner from "./components/Banner";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 function App() {
   
+  
 
   return (
-    <>
-       <>
-      <BrowserRouter>
-     
+    <BrowserRouter>
+      <div className="relative z-0">
         <div className="overflow-x-hidden">
-
-          <NavBar />     
-
+          <NavBar />  
         </div>
+        <Banner />
+
+        <Footer />
+      </div>
+
+    </BrowserRouter>
    
-         
-          
-          <Routes>
-            <Route path="/" element={<Home/>} />
+    
+    
+  );
+};
 
-        
-
-          </Routes>
- 
-
-     
-
-          <Footer/>
-          
-       
-  
-      </BrowserRouter>
-    </>
-    </>
-  )
-}
-
-export default App
+export default App;
---
+++ src/components/logProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import dogImage from "../assets/DogCatNight.png";
+import servicesWork from "../assets/servicesWork.png";
+
+const LogProfile = () => {
+  return (
+    <div>logProfile</div>
+  )
+}
+
+export default LogProfile

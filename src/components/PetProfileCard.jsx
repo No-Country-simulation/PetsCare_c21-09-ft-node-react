6446f6c141e62a22d5c607946d83d8cc070e5039
mauroR5Helvecia <rosalesmauro369@gmail.com>
2024-10-23 12:34:53 -0300

    add components contacto and about

--- src/components/PetProfileCard.jsx
+++ src/components/PetProfileCard.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { urlImage } from '../js/globalApi';
-import VistaEnlacesAdminUserPet from "../components/VistaEnlaceAdminUserPet";
 import VistaEnlaceAdminUserPet from '../components/VistaEnlaceAdminUserPet';
 
 function PetProfileCard({ pet }) {

39a78dc0a00b47fbaa14c3a9f0b27526ddf34c9d
WalterM <98977639+Mosow357@users.noreply.github.com>
2024-10-24 18:12:32 -0300

    add two cards users to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-
+import BannerLogIn from "../components/BannerLogIn"
 
 export default function Home() {
   return (
     <>
         <div className="container-home">
          Home component, aca agregar los componentes que queremos que se vean al entrar al sitio
+         <BannerLogIn />
         </div>
     </>
   )

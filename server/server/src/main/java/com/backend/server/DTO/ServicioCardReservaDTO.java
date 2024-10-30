2ca3b22710999134ac24cb7b5e9d2839216d3ee2
mauroR5Helvecia <rosalesmauro369@gmail.com>
2024-10-30 15:49:47 -0300

    modified method back end MapServicioCardReservadto REPLACE eNUM, _ space

--- server/server/src/main/java/com/backend/server/DTO/ServicioCardReservaDTO.java
+++ server/server/src/main/java/com/backend/server/DTO/ServicioCardReservaDTO.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class ServicioCardReservaDTO {
 
     private Long idServicio;
-    private EnumNombreServicio nombreServicio;
+    private String nombreServicio;
     private  String nombreComercio;
     private String imagenServicio;
     private boolean lugarFisico;
